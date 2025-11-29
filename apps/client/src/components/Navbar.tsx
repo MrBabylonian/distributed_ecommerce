00e92ddfe0dbdae3f88f97dc6f9d82093db6b5a6
MrBabylonian <bedirhangilgiler@gmail.com>
2025-11-29 15:12:20 +0100

    feat: initialize Next.js client application with Clerk authentication, middleware, and core UI components.

--- apps/client/src/components/Navbar.tsx
+++ apps/client/src/components/Navbar.tsx
@@ -3,35 +3,54 @@ import Link from "next/link";
 import SearchBar from "./SearchBar";
 import { Bell, Home, ShoppingCart } from "lucide-react";
 import ShoppingCartIcon from "./ShoppingCartIcon";
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
-  return (
-    <nav className="w-full flex items-center justify-between border-b border-gray-200 pb-4">
-      {/* LEFT */}
-      <Link href="/" className="flex items-center">
-        <Image
-          src="/logo.png"
-          alt="TrendLama"
-          width={36}
-          height={36}
-          className="w-6 h-6 md:w-9 md:h-9"
-        />
-        <p className="hidden md:block text-md font-medium tracking-wider">
-          TRENDLAMA.
-        </p>
-      </Link>
-      {/* RIGHT */}
-      <div className="flex items-center gap-6">
-        <SearchBar />
-        <Link href="/">
-          <Home className="w-4 h-4 text-gray-600"/>
-        </Link>
-        <Bell className="w-4 h-4 text-gray-600"/>
-        <ShoppingCartIcon/>
-        <Link href="/login">Sign in</Link>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="w-full flex items-center justify-between border-b border-gray-200 pb-4 ">
+            {/* LEFT */}
+            <Link href="/" className="flex items-center">
+                <Image
+                    src="/logo.png"
+                    alt="TrendLama"
+                    width={36}
+                    height={36}
+                    className="w-6 h-6 md:w-9 md:h-9"
+                />
+                <p className="hidden md:block text-md font-medium tracking-wider">
+                    TRENDLAMA.
+                </p>
+            </Link>
+            {/* RIGHT */}
+            <div className="flex items-center gap-6">
+                <SearchBar />
+                <Link href="/">
+                    <Home className="w-4 h-4 text-gray-600" />
+                </Link>
+                <Bell className="w-4 h-4 text-gray-600" />
+                <ShoppingCartIcon />
+                <div className="flex items-center gap-2.5">
+                    <SignedOut>
+                        <SignInButton>
+                            <button className="bg-amber-400 hover:scale-110 transition-transform
+                        hover:translate-y-[-1px] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                                Sign In
+                            </button>
+                        </SignInButton>
+                        <SignUpButton>
+                            <button className="bg-amber-200 hover:scale-110 transition-transform
+                        hover:translate-y-[-1px] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                                Sign Up
+                            </button>
+                        </SignUpButton>
+                    </SignedOut>
+                    <SignedIn>
+                        <UserButton />
+                    </SignedIn>
+                </div>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
